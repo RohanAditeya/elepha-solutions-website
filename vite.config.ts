3c089206feab1e8c390b49c9e9314650bbb7dbef
Rohan Aditeya <68320648+RohanAditeya@users.noreply.github.com>
2025-06-30 23:44:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/elepha-solutions-website/',
   plugins: [
     react(),
     tailwindcss(),
