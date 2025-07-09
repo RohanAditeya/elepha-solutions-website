ce6a016bd1fb0198483d06b5adb0abb0560fa6a0
rohan-aditeya <rohan.aditeya@gmail.com>
2025-07-09 23:04:47 +0530

    Added dropdown

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,10 +7,18 @@ const Header = () => {
             <Link to="/"><img className='logo' src={logoUrl} alt="Elepha solutions logo"/></Link>
             <div className='navButtons'>
                 <div className='aboutUsNavButton'>
-                    <p><a href='#about-us-section'>About Us</a></p>
+                    <p><Link to='/about'>About Us</Link></p>
                 </div>
                 <div className='servicesNavButton'>
                     <p><a href='#service-section'>Services</a></p>
+                    <div className='dropdown-content'>
+                        <ul>
+                            <li><Link to="/services/it-consulting">IT Consulting</Link></li>
+                            <li><Link to="/services/interview-as-a-service">Interview As a Service</Link></li>
+                            <li><Link to="/services/recruitment-as-a-service">Recruitment As a Service</Link></li>
+                            <li><Link to="/services/technical-workshops">Technical Workshops</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </header>
